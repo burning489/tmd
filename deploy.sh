7e91412a7285ed0de59d5d2ee7cae239a48c3ef2
dz <zd1998@whu.edu.cn>
2021-10-17 03:39:23 +0800

    fix(deploy): remove comparison on r_tol to save half of tasks

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-var1=(power rayleigh LOBPSD LOBPCG)
+var1=(power rayleigh LOBPCG)
 var2=(euler bb)
 var3=(1 2 Inf)
-var4=(1e-2 1e-1)
+# var4=(1e-2 1e-1)
 
 for var1i in "${var1[@]}"; do
     for var2i in "${var2[@]}"; do
         for var3i in "${var3[@]}"; do
-            for var4i in "${var4[@]}"; do
-                params="options.subspace_scheme=\"$var1i\";options.step_scheme=\"$var2i\";options.norm_scheme=\"$var3i\";options.r_tol=$var4i;"
-                ./task.sbatch $params
-            done
+            # for var4i in "${var4[@]}"; do
+            # params="options.subspace_scheme=\"$var1i\";options.step_scheme=\"$var2i\";options.norm_scheme=\"$var3i\";options.r_tol=$var4i;"
+            params="options.subspace_scheme=\"$var1i\";options.step_scheme=\"$var2i\";options.norm_scheme=\"$var3i\";"
+            sbatch task.sbatch $params
+            # done
         done
     done
 done
