715419fc7411e2bb7fdfa78c7936c9087a90a20a
dz <zd1998@whu.edu.cn>
2021-10-17 13:35:06 +0800

    chore: move timestamp to outer script

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,8 @@ for var1i in "${var1[@]}"; do
             # for var4i in "${var4[@]}"; do
             # params="options.subspace_scheme=\"$var1i\";options.step_scheme=\"$var2i\";options.norm_scheme=\"$var3i\";options.r_tol=$var4i;"
             params="options.subspace_scheme=\"$var1i\";options.step_scheme=\"$var2i\";options.norm_scheme=\"$var3i\";"
-            sbatch task.sbatch $params
+            time=$(date +"%y%m%d-%H%M%S")
+            sbatch task.sbatch $params $time
             sleep 2
             # done
         done
---
+++ t.sh
@@ -0,0 +1,5 @@
+for((i=0;i<5;i=i+1)); do
+    time=$(date +"%y%m%d-%H%M%S")
+    echo $time
+    sleep 1
+    done
